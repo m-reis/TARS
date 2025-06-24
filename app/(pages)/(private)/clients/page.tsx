e38858fd5b66f9ad4edfaff69b0d1c4b7c92c14b
Marcelo Cristo <marcelo.deftones@gmail.com>
2025-06-24 02:27:48 -0300

    readicionado dataGrid de clientes

--- app/(pages)/(private)/clients/page.tsx
+++ app/(pages)/(private)/clients/page.tsx
@@ -1,9 +1,102 @@
+'use client'
+
 import FredGrugPage from "@/app/components/fredGrugPage";
+import { Button, Container, IconButton } from '@mui/material';
+import ClientsCards from '@/app/components/clients/clientsCards'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Delete, Edit, PersonAdd, GroupRemove, Block } from '@mui/icons-material';
 
-export default async function Home(){
-    await new Promise(resolve => setTimeout(resolve, 5000));
+const columns: GridColDef<(typeof rows)[number]>[] = [
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+    disableColumnMenu: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+    disableColumnMenu: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    flex: 1,
+    disableColumnMenu: true,
+    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+  },
+  {
+    field: ' ',
+    align: 'center',
+    headerName: 'actions',
+    width: 100,
+    sortable: false,
+    filterable: false,
+    disableReorder: true,
+    renderCell: () => (
+        <>
+            <IconButton>
+                <Edit className='text-primary/80' />
+            </IconButton>
+            <IconButton>
+                <Delete className='text-red-500' />
+            </IconButton>
+        </>        
+    ),
+    renderHeader: () => (
+        <>
+            <IconButton>
+                <Block className='text-primary/80' />
+            </IconButton>
+            <IconButton>
+                <GroupRemove className='text-red-500' />
+            </IconButton>
+        </>        
+    )
+  }
+];
 
-    return (
-        <FredGrugPage />
-    );
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export default function Home() {
+
+  return (
+    <FredGrugPage>
+        <Container className="w-10/12 my-0 py-0  sm:mx-auto">
+            <ClientsCards />
+
+            <div className='flex justify-end'>
+                <Button className='right-0' variant="contained" startIcon={<PersonAdd />}>
+                    Cadastrar
+                </Button>
+            </div>
+
+            <Box className="rounded-xl my-4 overflow-hidden border border-gray-200" sx={{ maxHeight: 600}}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    checkboxSelection
+                    className='rounded text-primary font-cinzel'
+                    disableRowSelectionOnClick />
+            </Box>
+        </Container>
+    </FredGrugPage>
+  );
 }
