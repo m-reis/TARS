91c197bc2ee2cf45cf8375a841dde96a0ec07005
Marcelo Cristo <marcelo.deftones@gmail.com>
2025-07-23 01:17:10 -0300

    iniciado modelos de formularios

--- components/private/clients/DialogFormClients.tsx
+++ components/private/clients/DialogFormClients.tsx
@@ -1,6 +1,8 @@
 import DialogDefault from '@/components/DialogDefault';
 import { Client } from '@models/iClient';
-import { Button, DialogActions, TextField } from '@mui/material';
+import { AccountCircle, WhatsApp } from '@mui/icons-material';
+import { Box, Button, DialogActions, FormControl, Input, InputAdornment, InputLabel, TextField } from '@mui/material';
+
 
 export default function DialogFormClients( { client, modaStateOpened, handleClose }: { client?: Client, modaStateOpened : boolean, handleClose : () => void } ){
     
@@ -11,12 +13,49 @@ export default function DialogFormClients( { client, modaStateOpened, handleClos
         <>
             <DialogDefault size='sm' title="Formulário de Clientes" opened={modaStateOpened} hadleOpen={() => {}} handleClose={handleClose} >
                 <form className='px-5 pt-4'>
-                    <TextField required id="nome" name="nome" autoComplete="off" label="Nome" fullWidth variant="standard"
+                    <div className='flex items-center' >
+                        <AccountCircle className='mr-1'  />
+                        <TextField fullWidth autoComplete='off' label="Nome" variant="standard" />
+                    </div>
+
+                    <div className='flex items-center mt-3' >
+                        <WhatsApp className='mr-1'  />
+                        <TextField fullWidth autoComplete='off' label="Whatsapp" variant="standard" />
+                    </div>
+
+                    <TextField
+                        className='mt-5'
+                        id="input-with-icon-textfield"
+                        label="Nome"
+                        fullWidth
                         slotProps={{
-                            input: {
-                                className: 'text-sm'
-                            }
-                        }} />
+                        input: {
+                            startAdornment: (
+                            <InputAdornment position="start">
+                                <AccountCircle />
+                            </InputAdornment>
+                            ),
+                        },
+                        }}
+                        variant="standard"
+                    />
+
+                    <TextField
+                        className='mt-3'
+                        id="input-with-icon-textfield"
+                        label="Whatsapp" 
+                        fullWidth
+                        slotProps={{
+                        input: {
+                            startAdornment: (
+                            <InputAdornment position="start">
+                                <WhatsApp />
+                            </InputAdornment>
+                            ),
+                        },
+                        }}
+                        variant="standard"
+                    />
                 </form>
                 <DialogActions className='pt-3'>
                     <Button color='error'>Fechar</Button>
